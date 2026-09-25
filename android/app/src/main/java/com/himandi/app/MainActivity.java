fd72003a344986da72e494cf90c3330ff8ad6bb9
ssFAFE <midokeshk67@gmail.com>
2026-09-25 18:23:29 +0000

    Configure PWA manifest and ready for installation

---
+++ android/app/src/main/java/com/himandi/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.himandi.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.himandi.app',
+  appName: 'Him And I',
+  webDir: 'dist'
+};
+
+export default config;
